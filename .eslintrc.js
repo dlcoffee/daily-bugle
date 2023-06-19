7e5642cd126c7788d5a60672776d31e5b198367a
daniel tea <daniel.l.tea@gmail.com>
2023-06-18 23:24:47 -0700

    add dev deps

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	env: {
+		node: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'prettier'],
+}
