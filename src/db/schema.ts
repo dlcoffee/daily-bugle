9d483cdecbbb41d1e12a77e3c405a37ef58c666d
daniel tea <daniel.l.tea@gmail.com>
2023-06-29 23:44:37 -0700

    add permissions as a string because sqlite doesnt have jsonb

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -26,6 +26,7 @@ export const users = sqliteTable('users', {
   id: integer('id').primaryKey(), // 'id' is the column name
   username: text('username').notNull(),
   password: text('password'),
+  permissions: text('permissions'),
   createdAt: integer('created_at', { mode: 'timestamp' })
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -3,22 +3,34 @@ import { db } from './index'
 
 import { createUser } from '../users/services'
 import { createPost } from '../posts/services'
-;(async () => {
-  db.run(sql`DELETE FROM posts`)
-  db.run(sql`DELETE FROM users`)
+  ; (async () => {
+    db.run(sql`DELETE FROM posts`)
+    db.run(sql`DELETE FROM users`)
 
-  await createUser({
-    username: 'jjjameson',
-    password: 'menace',
-  })
+    await createUser({
+      username: 'jjjameson',
+      password: 'menace',
+      permissions: [
+        'system_admin',
+        'posts.create',
+        'posts.read',
+        'posts.update',
+        'posts.delete',
+        'users.create',
+        'users.read',
+        'users.update',
+        'users.delete',
+      ].join(','),
+    })
 
-  const pparker = await createUser({
-    username: 'pparker',
-    password: 'joespizza',
-  })
+    const pparker = await createUser({
+      username: 'pparker',
+      password: 'joespizza',
+      permissions: ['posts.create', 'posts.read', 'posts.update', 'posts.delete'].join(','),
+    })
 
-  await createPost({
-    message: 'hi my name is peter',
-    authorId: pparker.id,
-  })
-})()
+    createPost({
+      message: 'hi my name is peter',
+      authorId: pparker.id,
+    })
+  })()
